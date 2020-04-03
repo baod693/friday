02996169d31e445e1def9ef485300fe2d9c63981
Beau Dinh <baod693@gmail.com>
2020-04-03 11:36:01 -0400

    Commit2

--- Friday.py
+++ Friday.py
@@ -1,12 +1,28 @@
-user_num = int(input("Enter a number" ))
+num = int(input("Ask for a number "))
+i = 1
 
-for num in range(1, user_num):
-    if num % 3 == 0 and num % 5 == 0:
-        print('FizzBuzz')
-    elif num % 3 == 0:
-        print('Fizz')
-    elif num % 5 == 0:
-        print('Buzz')
-    else:
-        print(num)
+while i <= num:
+  #print (i) 
+  if i % 3 == 0 and i % 5 == 0:
+    print ("fizzbuzz")
+  elif i % 5 == 0:
+    print ("buzz")
+  elif i % 3 == 0:
+    print ("fizz")
+  else: 
+    print (i)
+  i += 1 
+
+  
+# user_num = int(input("Enter a number" ))
+
+# for num in range(1, user_num):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print('FizzBuzz')
+#     elif num % 3 == 0:
+#         print('Fizz')
+#     elif num % 5 == 0:
+#         print('Buzz')
+#     else:
+#         print(num)
     
